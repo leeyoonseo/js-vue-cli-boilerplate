6d20f74d13f68bf72cb57efacabc3674a7d15947
leeyoonseo <okayoon.lee@gmail.com>
2022-04-09 19:30:04 +0900

    Lint 설정 (eslint, stylelint, settings)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,51 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        printWith: 80,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        arrowParens: 'avoid',
+        bracketSpacing: true,
+        endOfLine: 'auto',
+        htmlWhitespaceSensitivity: 'css',
+        trailingComma: 'es5',
+        requirePragma: false,
+        insertPragma: false,
+        proseWrap: 'preserve',
+        vueIndentScriptAndStyle: false,
+      },
+    ],
+    'default-case': 'warn',
+    'prefer-destructuring': ['warn', { array: false }],
+    'no-magic-numbers': 0,
+    'import/namespace': 0,
+    'no-unused-vars': ['warn', {}],
+    'vue/no-parsing-error': [0, {}],
+    'no-lonely-if': 0,
+    'vue/attribute-hyphenation': [
+      'always' | 'never',
+      {
+        ignore: ['maxLength'],
+      },
+    ],
   },
 };
